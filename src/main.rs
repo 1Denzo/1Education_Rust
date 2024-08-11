e8d7173d5e71f0177b1a88097e91ac4ed4595850
denzo <denis_zotov@inbox.ru>
2024-08-11 15:47:31 +0500

    end of chapter

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
-
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
-}
 
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!(
-        "Площадь прямоугольника равняется {} квадратных точек.",
-        rect1.area()
-    );
+    println!("Прямоугольник rect1 может вместить rect2? {}", rect1.can_hold(&rect2));
+    println!("Прямоугольник rect1 вмещает rect3? {}", rect1.can_hold(&rect3));
+    println!("Площадь rect1 прямоугольника, {}", rect1.area());
+    println!("Площадь rect2 прямоугольника, {}", rect2.area());
+    println!("Площадь rect3 прямоугольника, {}", rect3.area());
 }
